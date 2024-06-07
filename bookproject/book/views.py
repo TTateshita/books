33cf023714cfb0c791e50f036143595dc78a4c41
TTateshita <t-tateshita@read.co.jp>
2024-06-07 17:29:16 +0900

    本レビュー機能全搭載

--- bookproject/book/views.py
+++ bookproject/book/views.py
@@ -1,41 +1,102 @@
-from django.shortcuts import render, redirect
+from typing import Any
+from django.db import models
+from django.forms.models import BaseModelForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
-from django.contrib.auth import logout
-from django.urls import reverse_lazy
-from .models import Book
+from django.urls import reverse_lazy, reverse
+from django.db.models import Avg
+from .models import Book, Review
+from django.core.paginator import Paginator
+from .consts import ITEM_PER_PAGE
+
+
 
 # Create your views here.
 def index_view(request):
-    object_list = Book.objects.order_by("category")
-    return render(request, "book/index.html", {"object_list":object_list})
+    object_list = Book.objects.order_by("-id")
+    ranking_list = Book.objects.annotate(avg_rate=Avg("review__rate")).order_by("-avg_rate") 
+    
+    paginator = Paginator(ranking_list, ITEM_PER_PAGE)
+    page_number = request.GET.get("page", 1)
+    page_obj = paginator.page(page_number)
+    
+    return render(
+        request,
+        "book/index.html", 
+        {"object_list":object_list, "ranking_list":ranking_list, "page_obj":page_obj },
+    )
 
-class ListBookView(ListView):
+
+class ListBookView(LoginRequiredMixin,ListView):
     template_name = "book/book_list.html"
     model = Book
+    paginate_by = ITEM_PER_PAGE
     
     
-class DetailBookView(DetailView):
+class DetailBookView(LoginRequiredMixin,DetailView):
     template_name = "book/book_detail.html"
     model = Book
     
 
-class CreateBookView(CreateView):
+class CreateBookView(LoginRequiredMixin,CreateView):
     template_name = "book/book_create.html"
     model = Book
-    fields = ("title", "text", "category")
+    fields = ("title", "text", "category", "thumbnail")
     success_url = reverse_lazy("list-book")
     
-class DeleteBookView(DeleteView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        
+        return super().form_valid(form)
+    
+class DeleteBookView(LoginRequiredMixin,DeleteView):
     template_name = "book/book_confirm_delete.html"
     model = Book
     success_url = reverse_lazy("list-book")
     
-class UpdateBookView(UpdateView):
-    template_name = "book/book_update.html"
+    def get_object(self, queryset=None):     #自分が書いた投稿だけ削除できる
+        obj = super().get_object(queryset)
+        
+        if obj.user != self.request.user:   
+            raise PermissionDenied
+        
+        return obj
+    
+class UpdateBookView(LoginRequiredMixin,UpdateView):
     model = Book
-    fields = ("title", "text", "category")
-    success_url = reverse_lazy("list-book")
-
-def logout_view(request):
-    logout(request)
-    return redirect("index")
+    fields = ("title", "text", "category", "thumbnail")
+    template_name = "book/book_update.html"
+    
+    def get_object(self, queryset=None):     #自分が書いた投稿だけ編集できる
+        obj = super().get_object(queryset)
+        
+        if obj.user != self.request.user:
+            raise PermissionDenied
+        
+        return obj
+    
+    def get_success_url(self):
+        return reverse("detail-book", kwargs={"pk": self.object.id})
+    
+class CreateReviewView(LoginRequiredMixin,CreateView):
+    model = Review
+    fields = ("book", "title", "text", "rate")
+    template_name = "book/review_form.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["book"] = Book.objects.get(pk=self.kwargs["book_id"])
+        return context
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse("detail-book", kwargs={'pk':self.object.book.id})
+    
+    
--- bookproject/bookproject/settings.py
+++ bookproject/bookproject/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book',
+    'book.apps.BookConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -106,9 +107,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "list-book"
+LOGIN_REDIRECT_URL = "index"
+
+LOGOUT_REDIRECT_URL = "index"
